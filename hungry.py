7371dd58d941858d85fe746190bf4959ff43e545
dadhichm <mohit.dadhich.min16@itbhu.ac.in>
2020-07-13 15:43:43 +0530

    updated

--- hungry.py
+++ hungry.py
@@ -1,13 +1,10 @@
 hungry= input('Are u hungry?')
 if hungry== 'yes':
     print('eat poha and namkeen')
-
     print('eat salads and friuts')
     print('do homework after eating')
 else:
     print('do homework')
     print('take rest after doing work')
-
-
     print('call me after taking rest')
 
